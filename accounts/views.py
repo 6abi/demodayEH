7b06b7a4464554b92d80f94087729be45a50211d
Barbara <6abi.cardoso@gmail.com>
2019-07-14 16:55:56 -0300

    adicionando nova div no register

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-# Create your views here.
+#Create your views here.
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/register.html"
+# #função de cadastro do usuario
+# def register(request):
+#     form = UserCreationForm()
+#     return render(request, 'registration/register.html', {'form': form})
