1dae1fc6e162219d6e829063ea7e8db1da0f50ba
Barbara <6abi.cardoso@gmail.com>
2019-07-16 03:04:23 -0300

    Criação do arquivo requiriments, Procfile, para integrar projeto do git com o heroku

--- bike/settings.py
+++ bike/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,4 +129,4 @@ LOGIN_REDIRECT_URL ='/../rotas/usuario'
 
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-
+django_heroku.settings(locals())
